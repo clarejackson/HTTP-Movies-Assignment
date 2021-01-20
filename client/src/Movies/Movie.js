4099fdddd5d09bb0dde1d21332a9752c2f79e97a
Clare Jackson <clare-jackson@lambdastudents.com>
2021-01-20 15:14:46 -0500

    Update and delete complete.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
 function Movie({ addToSavedList }) {
+  const { push } = useHistory();
   const [movie, setMovie] = useState(null);
   const params = useParams();
+  const id = params.id;
 
   const fetchMovie = (id) => {
     axios
@@ -26,12 +28,30 @@ function Movie({ addToSavedList }) {
     return <div>Loading movie information...</div>;
   }
 
+  const deleteMovie = () => {
+    axios
+    .delete(`http://localhost:5000/api/movies/${movie.id}`)
+    .then(res => { 
+      console.log(res);
+      push("/")
+    })
+    .catch(err => console.log(err))
+  }
+
   return (
     <div className="save-wrapper">
       <MovieCard movie={movie} />
 
+      <div onClick={() => push(`/update-movie/${params.id}`)}>
+        Edit
+        </div>
+
       <div className="save-button" onClick={saveMovie}>
         Save
+      </div>
+
+      <div onClick={deleteMovie}>
+        Delete
       </div>
     </div>
   );
